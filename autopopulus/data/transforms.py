fix target encoding and updated tests (slightly wip) simplified continuous scaling

autopopulus/data/transforms.py
@@ -314,25 +314,37 @@ class Discretizer(TransformerMixin, BaseEstimator):
         return df
 
 
-class ScaleContinuous(TransformerMixin, BaseEstimator):
+class ColTransformPandas(TransformerMixin, BaseEstimator):
     """
-    Wrapper class to ColumnTransformer.
+    Wrapper class to ColumnTransformer to scale only continuous columns.
     Allows us to also return to pandas and restore original column order.
     TODO: write tests
     """
 
-    def __init__(self, orig_cols: List[str], *args, **kwargs) -> None:
+    def __init__(
+        self, orig_cols: List[str], enforce_numpy: bool = False, *args, **kwargs
+    ) -> None:
         super().__init__()
         self.orig_cols = orig_cols
-        self.column_tsfm = ColumnTransformer(*args, **kwargs)
+        self.enforce_numpy = enforce_numpy  # TODO: might not be necessary
+        self.column_tsfm = ColumnTransformer(
+            *args, remainder="passthrough", verbose_feature_names_out=False, **kwargs
+        )
 
     def fit(self, X: np.ndarray, y: pd.Series):
         # Enforce numpy so we don't get "UserWarning: X does not have valid feature names, but MinMaxScaler was fitted with feature names"
-        self.column_tsfm.fit(enforce_numpy(X), y)
+        if self.enforce_numpy:
+            self.column_tsfm.fit(enforce_numpy(X), y)
+        else:
+            self.column_tsfm.fit(X, y)
+
         return self
 
     def transform(self, X: pd.DataFrame) -> Tuple[np.ndarray, List[str], pd.Index]:
-        Xt = self.column_tsfm.transform(enforce_numpy(X))
+        if self.enforce_numpy:  # Need to retain access to X as df so don't overwrite
+            Xt = self.column_tsfm.transform(enforce_numpy(X))
+        else:
+            Xt = self.column_tsfm.transform(X)
         # without original cols it's going to have meaningless feature names like x7
         out_of_order_cols = self.column_tsfm.get_feature_names_out(self.orig_cols)
         df = pd.DataFrame(Xt, columns=out_of_order_cols, index=X.index)
@@ -409,20 +421,23 @@ def invert_target_encoding_tensor(
     encoded_data = encoded_data.detach().numpy()  # needed for inverse transform sklearn
 
     # this is in collapsed-onehot space
-    mapping = mean_to_ordinal_map["mapping"]
     for idx, mapping in mean_to_ordinal_map["mapping"].items():
-        for k, v in mapping.items():
-            encoded_data[:, idx] = np.where(
-                encoded_data[:, idx] == k, v, encoded_data[:, idx]
-            )
+        mean_encoded_values = np.array(list(mapping.values()))
+        ordinal_value = np.array(list(mapping.keys()))
+        # get nearest since after autoencoder i wont' have the exact continuous value
+        # also works if multiple categories have the same encoding
+        # if there's multiple mins prefers the 1st one (selects an actual category over nan if they're all the same)
+        encoded_data[:, idx] = np.array(
+            [
+                ordinal_value[np.abs(mean_encoded_values - v).argmin()]
+                for v in encoded_data[:, idx]
+            ]
+        )
 
     # Need pd if category was string
     encoded_data = pd.DataFrame(encoded_data, columns=combined_onehot_columns)
     # can only un-ordinal encode the ordinally encoded columns
-    ordinal_enc_cols = list(mean_to_ordinal_map["mapping"].keys())
-    encoded_data.iloc[:, ordinal_enc_cols] = mean_to_ordinal_map["inverse_transform"](
-        encoded_data.iloc[:, ordinal_enc_cols]
-    )
+    encoded_data = mean_to_ordinal_map["inverse_transform"](encoded_data)
 
     # explode columns if onehots were flattened
     if combined_onehots_groupby is not None:
@@ -430,7 +445,9 @@ def invert_target_encoding_tensor(
         return torch.tensor(
             onehot_multicategorical_column(combined_onehots_groupby.values())(
                 encoded_data
-            )[original_columns].values
+            )
+            .reindex(columns=original_columns)  # will fill 0s for missing cats
+            .values
         ).float()
 
     # reorder to match original

autopopulus/test/test_data.py
@@ -298,7 +298,7 @@ class TestCommonDataModule(unittest.TestCase):
                                     data_name
                                 ].__self__.named_steps.keys()
                             ),
-                            ["enforce-numpy", "continuous-scale", "enforce-pandas"],
+                            ["scale_continuous"],
                         )
 
                     np.testing.assert_array_equal(
@@ -413,7 +413,7 @@ class TestCommonDataModule(unittest.TestCase):
                                     data_name
                                 ].__self__.named_steps.keys()
                             ),
-                            ["combine_onehots", "target_encode"],
+                            ["combine_onehots", "target_encode_categorical"],
                         )
 
                         # The onehot_groupby and columns passed to combineonehots should be in original space
@@ -434,7 +434,7 @@ class TestCommonDataModule(unittest.TestCase):
                         # The categorical columns for the targetencoder should be in combined space
                         passed_cat_cols = (
                             data.transforms["mapped"][data_name]
-                            .__self__.named_steps["target_encode"]
+                            .__self__.named_steps["target_encode_categorical"]
                             .cols
                         )
                         np.testing.assert_array_equal(
@@ -463,7 +463,7 @@ class TestCommonDataModule(unittest.TestCase):
                                 data_name
                             ].__self__.named_steps.keys()
                         ),
-                        ["enforce-numpy", "continuous-scale", "enforce-pandas"],
+                        ["scale_continuous"],
                     )
                 for data_name in ["data", "ground_truth"]:
                     self.assertEqual(
@@ -473,11 +473,9 @@ class TestCommonDataModule(unittest.TestCase):
                             ].__self__.named_steps.keys()
                         ),
                         [
-                            "enforce-numpy",
-                            "continuous-scale",
-                            "enforce-pandas",
+                            "scale_continuous",
                             "combine_onehots",
-                            "target_encode",
+                            "target_encode_categorical",
                         ],
                     )
 
@@ -584,7 +582,7 @@ class TestCommonDataModule(unittest.TestCase):
                                     data_name
                                 ].__self__.named_steps.keys()
                             ),
-                            ["enforce-numpy", "continuous-scale", "enforce-pandas"],
+                            ["scale_continuous"],
                         )
 
                     np.testing.assert_array_equal(
@@ -677,13 +675,13 @@ class TestCommonDataModule(unittest.TestCase):
                                     data_name
                                 ].__self__.named_steps.keys()
                             ),
-                            ["target_encode"],
+                            ["target_encode_categorical"],
                         )
 
                         # The categorical columns for the targetencoder should be the same as the categorical cols for original data
                         passed_cat_cols = (
                             data.transforms["mapped"][data_name]
-                            .__self__.named_steps["target_encode"]
+                            .__self__.named_steps["target_encode_categorical"]
                             .cols
                         )
                         self.assertEqual(passed_cat_cols, hypothesis["cat_cols"])
@@ -708,7 +706,7 @@ class TestCommonDataModule(unittest.TestCase):
                                 data_name
                             ].__self__.named_steps.keys()
                         ),
-                        ["enforce-numpy", "continuous-scale", "enforce-pandas"],
+                        ["scale_continuous"],
                     )
                 for data_name in ["data", "ground_truth"]:
                     self.assertEqual(
@@ -718,10 +716,8 @@ class TestCommonDataModule(unittest.TestCase):
                             ].__self__.named_steps.keys()
                         ),
                         [
-                            "enforce-numpy",
-                            "continuous-scale",
-                            "enforce-pandas",
-                            "target_encode",
+                            "scale_continuous",
+                            "target_encode_categorical",
                         ],
                     )
 

autopopulus/test/test_transforms.py
@@ -231,9 +231,9 @@ class TestTransforms(unittest.TestCase):
 
             categories = [[0, 1], [0, 1, 2, 3], [0, 1, 2]]
             # mock an encoding for each categorical column
-            mean_to_ordinal_map = {
+            ordinal_to_mean_map = {
                 combined_df.columns.get_loc(col): {
-                    rng.random(): category for category in categories[i]
+                    category: rng.random() for category in categories[i]
                 }
                 for i, col in enumerate(
                     hypothesis["onehot"]["bin_cols"] + hypothesis["onehot_prefixes"]
@@ -245,7 +245,7 @@ class TestTransforms(unittest.TestCase):
                 onehot_df.astype(float),
                 ["bin", "mult1", "mult2"],
                 categories,
-                mean_to_ordinal_map,
+                ordinal_to_mean_map,
                 onehot_df.columns,
                 combined_groupby,
             )
@@ -385,9 +385,9 @@ class TestTransforms(unittest.TestCase):
             assume(not df.empty)
             categories = [[0, 1], [0, 1, 2, 3], [0, 1, 2]]
             # mock an encoding for each categorical column
-            mean_to_ordinal_map = {
+            ordinal_to_mean_map = {
                 df.columns.get_loc(col): {
-                    rng.random(): category for category in categories[i]
+                    category: rng.random() for category in categories[i]
                 }
                 for i, col in enumerate(hypothesis["cat_cols"])
             }
@@ -396,7 +396,7 @@ class TestTransforms(unittest.TestCase):
                 df,
                 hypothesis["cat_cols"],
                 categories,
-                mean_to_ordinal_map,
+                ordinal_to_mean_map,
                 df.columns,
             )
 
@@ -612,7 +612,7 @@ class TestTransforms(unittest.TestCase):
         true_df: pd.DataFrame,
         cat_cols: List[str],
         categories: List[List[int]],
-        mean_to_ordinal_map: Dict[str, Dict[float, Any]],
+        ordinal_to_mean_map: Dict[str, Dict[float, Any]],
         orig_cols: List[str],
         combined_onehots_groupby: Optional[Dict[int, int]] = None,
     ):
@@ -628,17 +628,21 @@ class TestTransforms(unittest.TestCase):
         # enc._missing_indices = {}
         # flip the mean_to_ordinal map to pretend like we're target encoding
         target_encode_map = {
-            df.columns[idx]: {cat: mean for mean, cat in mapping.items()}
-            for idx, mapping in mean_to_ordinal_map.items()
+            df.columns[idx]: mapping for idx, mapping in ordinal_to_mean_map.items()
         }
 
+        # only applying to cat_cols since im roundabout creating my own map/inverse transform instead of using targetencode since i cannot control the categories also bc i dont have labels
         unencoded_tensor = invert_target_encoding_tensor(
-            torch.tensor(df.replace(target_encode_map).values),
+            torch.tensor(df.replace(target_encode_map)[cat_cols].values),
             {
-                "inverse_transform": enc.inverse_transform,
-                "mapping": mean_to_ordinal_map,
+                "inverse_transform": lambda df: pd.DataFrame(
+                    enc.inverse_transform(df), columns=cat_cols
+                ).astype(float),
+                "mapping": {
+                    i: mapping for i, mapping in enumerate(ordinal_to_mean_map.values())
+                },
             },
-            df.columns,
+            cat_cols,
             orig_cols,
             combined_onehots_groupby,
         )