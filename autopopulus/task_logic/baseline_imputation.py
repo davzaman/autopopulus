2fe78ac6d688a5bafae93e8a5f456e4c147c49fa
Davina <davzaman@gmail.com>
2023-04-27 16:25:58 -0700

    reorder methods for readability

--- autopopulus/task_logic/baseline_imputation.py
+++ autopopulus/task_logic/baseline_imputation.py
@@ -15,6 +15,35 @@ from autopopulus.data import CommonDataModule
 from autopopulus.utils.impute_metrics import CWMAAPE, CWRMSE
 
 
+def baseline_imputation_logic(
+    args: Namespace, data: CommonDataModule
+) -> Dict[str, Dict[str, DataFrame]]:
+    """
+    Wrapper for Baseline static methods: setup dataset and logging.
+    """
+
+    if hasattr(baseline_static_imputation, args.method):
+        module = baseline_static_imputation
+    else:
+        module = baseline_longitudinal_imputation
+    fn = getattr(module, args.method)
+    # will create train/val/test
+    data.setup("fit")
+
+    imputed_data = fn(args, data)
+
+    ## LOGGING ##
+    if (
+        args.method != "none"
+    ):  # Logging here if baseline experiment (not fully observed)
+        log = BasicLogger(args=args, experiment_name=args.experiment_name)
+        # if args.fully_observed:
+        log_baseline_imputation_performance(imputed_data, data, log)
+        log.close()
+
+    return imputed_data
+
+
 def log_baseline_imputation_performance(
     results: Dict[str, DataFrame],
     data: CommonDataModule,
@@ -58,32 +87,3 @@ def log_baseline_imputation_performance(
                 },
                 tb_name_format="{step}/{split}-{name}-{subgroup}",
             )
-
-
-def baseline_imputation_logic(
-    args: Namespace, data: CommonDataModule
-) -> Dict[str, Dict[str, DataFrame]]:
-    """
-    Wrapper for Baseline static methods: setup dataset and logging.
-    """
-
-    if hasattr(baseline_static_imputation, args.method):
-        module = baseline_static_imputation
-    else:
-        module = baseline_longitudinal_imputation
-    fn = getattr(module, args.method)
-    # will create train/val/test
-    data.setup("fit")
-
-    imputed_data = fn(args, data)
-
-    ## LOGGING ##
-    if (
-        args.method != "none"
-    ):  # Logging here if baseline experiment (not fully observed)
-        log = BasicLogger(args=args, experiment_name=args.experiment_name)
-        # if args.fully_observed:
-        log_baseline_imputation_performance(imputed_data, data, log)
-        log.close()
-
-    return imputed_data
