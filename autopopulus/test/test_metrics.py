fix behavior for all torchmetrics under masking with no missing (ew + cw). the mean at the end needs to ignore completely observed columns and not incorporate it
instead of filling 0, just assert that whenever the denominator/total is 0, the sum error or count of correct is 0/1 respectively. if there are no items to compute an error then return 0/1 early.

autopopulus/test/test_metrics.py
@@ -405,15 +405,13 @@ class TestStaticMulticatContinuousMetrics(unittest.TestCase):
                 "Columns with no Missingness (1 Col with Missing Values)"
             ):
                 missing_indicators = torch.zeros_like(tensor_df).to(bool)
-                # make the first ctn column to be missing
-                missing_indicators[:, hypothesis["ctn_cols_idx"][0]] = True
+                # make the second ctn column to be missing
+                missing_indicators[:, hypothesis["ctn_cols_idx"][1]] = True
                 with self.subTest("RMSE"):
                     with self.subTest("CWRMSE"):
                         self.assertAlmostEqual(
                             (  # no obs values in 2nd column, 2 errors
-                                (
-                                    sqrt(((diffs[2] ** 2) + (diffs[3] ** 2)) / nsamples)
-                                )  # all other columns the same / 0 error
+                                (sqrt(((diffs[2] ** 2) + (diffs[3] ** 2)) / nsamples))
                                 / 1  # average over all 1 missing column
                             ),
                             self.rmse_colwise(
@@ -455,7 +453,7 @@ class TestStaticMulticatContinuousMetrics(unittest.TestCase):
                                 )
                                 / nsamples
                             ).item()
-                            / nfeatures  # average over al cols
+                            / 1  # average over the 1 missing col
                             * scale,  # scale
                             self.maape_colwise(
                                 error_df, tensor_df, missing_indicators
@@ -473,8 +471,8 @@ class TestStaticMulticatContinuousMetrics(unittest.TestCase):
                             (
                                 np.arctan(abs(diffs[2] / (true_at_err[2] + EPSILON)))
                                 + np.arctan(abs(diffs[3] / (true_at_err[3] + EPSILON)))
-                            )  # all columns observed except one
-                            / nsamples
+                            ).item()
+                            / nsamples  # all columns observed except one
                             * scale,  # scale
                             self.maape_elwise(
                                 error_df, tensor_df, missing_indicators
@@ -708,8 +706,8 @@ class TestStaticMulticatCategoricalMetrics(unittest.TestCase):
             missing_indicators[:, hypothesis["bin_cols_idx"][0]] = True
             self.assertAlmostEqual(
                 # nsamples is the missing col is all N, but 2 errors
-                # For the remaining F - 1, cols acc is 1
-                (((N - 2) / N) + (F - 1)) / F,
+                # For the remaining F - 1, we don't care bc they're observed
+                ((N - 2) / N) / 1,
                 self.accuracy_colwise(error_df, tensor_df, missing_indicators).item(),
                 places=WITHIN,
             )
@@ -960,9 +958,8 @@ class TestStaticOnehotCategoricalMetrics(unittest.TestCase):
             missing_indicators[:, hypothesis["onehot"]["onehot_cols_idx"][0]] = True
             self.assertAlmostEqual(
                 # nsamples is the missing col is all N, but 2 errors
-                # For the remaining F - 1, cols acc is 1
-                (((N - 2) / N) + (F - 1)) / F,
-                # (((N - 1 - 1) / (N - 1)) + F - 1) / F,
+                # For the remaining F - 1, they're all observed so we don't care
+                ((N - 2) / N) / 1,
                 self.accuracy_colwise(error_df, tensor_df, missing_indicators).item(),
                 places=WITHIN,
             )

autopopulus/utils/impute_metrics.py
@@ -109,14 +109,18 @@ class MAAPEMetric(Metric):
             (vs nan caused by 0/0 AKA where there's no missing values).
         This also works for EW when no data is missing.
         """
-        maape = maape.where(
-            ~((self.sum_errors == 0) & (self.total == 0)),
-            torch.tensor(0.0, device=self.sum_errors.device),
-        )
+        assert (
+            self.sum_errors[self.total == 0] == 0
+        ).all(), "Calculated sum error where there are no indicated values."
+        num_maape_items = self.total != 0
+        # no items to calc error over (due to indicators)
+        if not torch.sum(num_maape_items):
+            return torch.tensor(0.0, device=self.sum_errors.device)
         if self.scale_to_01:  # range [0, pi/2] scale to [0, 1]
             maape *= 2 / pi
-        if self.columnwise:
-            return torch.mean(maape)
+        if self.columnwise:  # mean, ignoring the unincluded cols
+            # sum the rmse for the cols we care about / # cols we care about
+            return torch.sum(maape[num_maape_items]) / torch.sum(num_maape_items)
         return maape
 
 
@@ -198,12 +202,17 @@ class RMSEMetric(Metric):
             (vs nan caused by 0/0 AKA where there's no missing values).
         This also works for EW when no data is missing.
         """
-        rmse = rmse.where(
-            ~((self.sum_errors == 0) & (self.total == 0)),
-            torch.tensor(0.0, device=self.sum_errors.device),
-        )
-        if self.columnwise:
-            return torch.mean(rmse)
+        assert (
+            self.sum_errors[self.total == 0] == 0
+        ).all(), "Calculated sum error where there are no indicated values."
+        num_rmse_items = self.total != 0
+        # no items to calc error over (due to indicators)
+        if not torch.sum(num_rmse_items):
+            return torch.tensor(0.0, device=self.sum_errors.device)
+
+        if self.columnwise:  # mean, ignoring the unincluded cols
+            # sum the rmse for the cols we care about / # cols we care about
+            return torch.sum(rmse[num_rmse_items]) / torch.sum(num_rmse_items)
         return rmse
 
 
@@ -293,12 +302,17 @@ class AccuracyMetric(Metric):
             (vs nan caused by 0/0 AKA where there's no missing values).
         This also works for EW when no data is missing.
         """
-        acc = acc.where(
-            ~((self.num_correct == 0) & (self.total == 0)),
-            torch.tensor(1.0, device=self.num_correct.device),
-        )
-        if self.columnwise:
-            return torch.mean(acc)
+        assert (
+            self.num_correct[self.total == 0] == 0
+        ).all(), "Calculated sum error where there are no indicated values."
+        num_acc_items = self.total != 0
+        # no items to calc accuracy over (due to indicators)
+        if not torch.sum(num_acc_items):
+            return torch.tensor(1.0, device=self.num_correct.device)
+
+        if self.columnwise:  # mean, ignoring the unincluded cols
+            # sum the rmse for the cols we care about / # cols we care about
+            return torch.sum(acc[num_acc_items]) / torch.sum(num_acc_items)
         return acc
 
 