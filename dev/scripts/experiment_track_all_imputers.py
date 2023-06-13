8a7bf384f77340f2d62b1e56f94c7836be9d1b2f
Davina <davzaman@gmail.com>
2023-06-13 11:02:52 -0700

    turn off uniform prob for discretize

--- dev/scripts/experiment_track_all_imputers.py
+++ dev/scripts/experiment_track_all_imputers.py
@@ -134,8 +134,8 @@ for dataset in datasets:
                     "feature-map": cli_str(feature_mapping_variational),
                     "replace-nan-with": cli_str(replace_nan_with),
                 }
-            if command_args.get("feature-map", "") == "discretize_continuous":
-                command_args["uniform-prob"] = True
+            # if command_args.get("feature-map", "") == "discretize_continuous":
+            # command_args["uniform-prob"] = cli_str(True)
 
             if all_data:
                 # When multiple flags have list values, Guild generates the cartesian product of all possible flag combinations.
