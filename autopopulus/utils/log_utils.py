32d4582d24599436e2161447ef9894ac61c47237
Davina <davzaman@gmail.com>
2023-06-28 02:40:43 -0700

    fix log_utils failing to merge properly

--- autopopulus/utils/log_utils.py
+++ autopopulus/utils/log_utils.py
@@ -1,13 +1,10 @@
 from argparse import Namespace
 from logging import FileHandler, StreamHandler, basicConfig, INFO
 from shutil import copy, rmtree, copytree
-from shutil import copy, rmtree, copytree
 from typing import Any, Dict, List, Optional, Union
 
 from cloudpickle import dump, load
-from cloudpickle import dump, load
 from regex import search
-from os.path import join, exists, dirname
 from os.path import join, exists, dirname
 from os import makedirs, walk
 import sys
@@ -26,8 +23,6 @@ from autopopulus.data.types import DataTypeTimeDim
 from autopopulus.utils.utils import rank_zero_print
 
 TUNE_LOG_DIR = "tune_results"
-# LOGGER_TYPE = "TensorBoard"
-LOGGER_TYPE = "mlflow"
 # LOGGER_TYPE = "TensorBoard"
 LOGGER_TYPE = "mlflow"
 # LOGGER_TYPE = "Aim"
@@ -200,8 +195,6 @@ class BasicLogger:
         elif LOGGER_TYPE == "Aim":
             # return AimRun(repo=target_path)
             self.logger = AimRun(run_hash=run_hash, experiment=experiment_name)
-            # return AimRun(repo=target_path)
-            self.logger = AimRun(run_hash=run_hash, experiment=experiment_name)
             self.logger["hparams"] = base_context
         elif LOGGER_TYPE == "mlflow":
             self.logger: MlFlowRun = mlflow.get_run(
@@ -282,32 +275,18 @@ class BasicLogger:
             logged_name = tb_name_format.format(name=name, **context)
         else:
             logged_name = name
-        if tb_name_format is not None:
-            logged_name = tb_name_format.format(name=name, **context)
-        else:
-            logged_name = name
 
         if self.verbose:
             if global_step is not None:
                 rank_zero_print(f"{logged_name}[{global_step}]: {metric}")
             else:
                 rank_zero_print(f"{logged_name}: {metric}")
-        if self.verbose:
-            if global_step is not None:
-                rank_zero_print(f"{logged_name}[{global_step}]: {metric}")
-            else:
-                rank_zero_print(f"{logged_name}: {metric}")
 
-        if isinstance(self.logger, SummaryWriter):
         if isinstance(self.logger, SummaryWriter):
             self.logger.add_scalar(logged_name, metric, global_step, walltime)
         elif isinstance(self.logger, AimRun):
             # this doesn't use logged_name
-        elif isinstance(self.logger, AimRun):
-            # this doesn't use logged_name
             self.logger.track(metric, name, global_step, context={**context})
-        elif isinstance(self.logger, MlFlowRun):
-            mlflow.log_metric(key=logged_name, value=metric, step=global_step)
         elif isinstance(self.logger, MlFlowRun):
             mlflow.log_metric(key=logged_name, value=metric, step=global_step)
 
@@ -346,15 +325,12 @@ class BasicLogger:
             if isinstance(self.logger, SummaryWriter):
                 self.logger.add_text(tag, text_string, global_step, walltime)
             elif isinstance(self.logger, AimRun):
-            elif isinstance(self.logger, AimRun):
                 self.logger.add_text(
                     Text(text_string),
                     tag,
                     global_step,
                     context={**context},
                 )
-            elif isinstance(self.loger, MlFlowRun):
-                mlflow.log_text(text_string, f"{tag}.txt")
             elif isinstance(self.loger, MlFlowRun):
                 mlflow.log_text(text_string, f"{tag}.txt")
 
@@ -392,18 +368,7 @@ def metric_path_from_run_id(run_id: str) -> str:
     )
 
 
-def metric_path_from_run_id(run_id: str) -> str:
-    return join(
-        dirname(mlflow.get_run(run_id).info.artifact_uri).replace("file://", ""),
-        "metrics",
-    )
-
-
 def copy_artifacts_from_tune(
-    best_result: air.Result,
-    model_path: str,
-    logger: Logger,
-    mlflow_callback: Optional[MLflowLoggerCallback] = None,
     best_result: air.Result,
     model_path: str,
     logger: Logger,
@@ -446,42 +411,11 @@ def copy_artifacts_from_tune(
             makedirs(metric_path)
 
     save_fn(join(best_result.checkpoint.path, "model"), model_path)
-    if isinstance(logger, MLFlowLogger):
-        # don't want a to_path, mlflow will figure it out
-        save_fn = lambda from_path, to_path: logger.experiment.log_artifact(
-            logger._run_id, from_path, to_path
-        )
-        keep_regex = ".*.json"
-        metric_path = None
-    else:
-        save_fn = copy
-        keep_regex = "tfevents|.*.json"
-        metric_path = logger.save_dir
-        if not exists(metric_path):
-            makedirs(metric_path)
-
-    save_fn(join(best_result.checkpoint.path, "model"), model_path)
 
     # copy metrics
     for root, dirs, files in walk(best_result.log_dir):
         if root == str(best_result.log_dir):  # only look at high level (not per rank)
             for file in files:  # only tfevent and json files
-                if search(keep_regex, file):
-                    save_fn(join(root, file), metric_path)
-
-    # above won't copy over metrics saved separately/synced to mlflow
-    # ray-tune will save it disjoint for each epoch
-    if isinstance(logger, MLFlowLogger):
-        # copy over metrics if mlflow
-        best_mlflow_run_id = {
-            str(trial): run_id for trial, run_id in mlflow_callback._trial_runs.items()
-        }[best_result.metrics["trial_id"]]
-        copytree(
-            metric_path_from_run_id(best_mlflow_run_id),
-            metric_path_from_run_id(logger._run_id),
-            dirs_exist_ok=True,
-        )
-
                 if search(keep_regex, file):
                     save_fn(join(root, file), metric_path)
 
